d050a3dec6291b8e4e958c25775048fcbcc9475a
aravind-balaji-28 <balajiaravind99@gmail.com>
2026-03-02 23:12:04 +0530

    update code

--- app/components/Portfolio/index.tsx
+++ app/components/Portfolio/index.tsx
@@ -30,30 +30,20 @@ export default function Portfolio() {
                     const isActive = activeIndex === index;
 
                     return (
-                        <div
-                            key={index}
-                            className="relative w-full max-w-[429px] aspect-[429/271] rounded-[10px] overflow-hidden shadow-2xl group"
-                        >
+                        <div key={index}
+                            className="relative w-full max-w-[429px] aspect-[429/271] rounded-[10px] overflow-hidden shadow-2xl group">
                             <div className="absolute inset-0 overflow-hidden">
-                                <img
-                                    src={card.img}
+                                <img src={card.img}
                                     alt={card.title}
-                                    style={{ display: "block" }}
-                                    className="absolute inset-0 w-full h-full object-cover block transition-transform duration-700 ease-[cubic-bezier(0.22,1,0.36,1)]"
-                                />
+                                    className="absolute inset-0 w-full h-full object-cover block transition-transform duration-700 ease-[cubic-bezier(0.22,1,0.36,1)]" />
                             </div>
 
                             <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent pointer-events-none" />
 
-                            <div
-                                className={`absolute inset-0 p-6 flex flex-col justify-center text-white
-                bg-black/95
-                transition-[clip-path] duration-700 ease-in-out
-                ${isActive
-                                        ? "[clip-path:circle(150%_at_20%_85%)]"
-                                        : "[clip-path:circle(0%_at_20%_85%)]"
-                                    }`}
-                            >
+                            <div className={`absolute inset-0 p-6 flex flex-col justify-center text-white bg-black/95 transition-[clip-path] duration-700 ease-in-out ${isActive
+                                ? "[clip-path:circle(150%_at_20%_85%)]"
+                                : "[clip-path:circle(0%_at_20%_85%)]"
+                                }`}>
                                 <h3 className="text-xl font-semibold mb-2">
                                     {card.title}
                                 </h3>
@@ -65,15 +55,10 @@ export default function Portfolio() {
                             <button
                                 onClick={() => setActiveIndex(isActive ? null : index)}
                                 className={`absolute -bottom-6 -left-6 w-[60px] h-[60px] rounded-full flex items-center justify-center text-white text-xl transition-all duration-500 ease-[cubic-bezier(0.22,1,0.36,1)] z-30 shadow-xl backdrop-blur-md border border-white/10 ${isActive
-                                        ? "bg-[#ff006e] shadow-pink-500/40"
-                                        : "bg-[#3a86ff] shadow-blue-500/40"
-                                    } hover:scale-110 active:scale-95`}
-                            >
-                                <span
-                                    className={`transition-transform duration-500 
-    ease-[cubic-bezier(0.22,1,0.36,1)]
-    ${isActive ? "rotate-180 scale-110" : "rotate-0 scale-100"}`}
-                                >
+                                    ? "bg-[#ff006e] shadow-pink-500/40"
+                                    : "bg-[#3a86ff] shadow-blue-500/40"
+                                    } hover:scale-110 active:scale-95`}>
+                                <span className={`mb-[18px] ml-[20px] transition-transform duration-500 ease-[cubic-bezier(0.22,1,0.36,1)] ${isActive ? "rotate-180 scale-110" : "rotate-0 scale-100"}`}>
                                     <FontAwesomeIcon className="rotate-180" size="xs" icon={isActive ? faUpRightAndDownLeftFromCenter : faDownLeftAndUpRightToCenter} />
                                 </span>
                             </button>
